1794caacd78007f2526f94994807f7b93a8d1257
Eliav Lavi <eliavlavi@gmail.com>
2018-02-21 19:37:25 +0200

    missed one file...

--- third/visit.rb
+++ third/visit.rb
@@ -1,27 +1,3 @@
-class Visa
-  attr_accessor :length_in_days, :used, :type
-
-  TYPES = PLEASURE_TYPES + BUSINESS_TYPES
-
-  PLEASURE_TYPES = [
-      A = 'a',
-      B = 'b'
-  ]
-
-  BUSINESS_TYPES = [
-      C = 'c',
-      D = 'd'
-  ]
-
-  def business_type?
-    BUSINESS_TYPES.include? @type
-  end
-
-  def pleasure_type?
-    PLEASURE_TYPES.include? @type || @type.nil?
-  end
-end
-
 class Visit
   def initialize(length_in_days, is_business_trip, visa)
     @length_in_days = length_in_days
