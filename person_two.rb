f3a068ee1a548c8529c17fc416ea48fbb3dc11e2
Eliav Lavi <eliavlavi@gmail.com>
2018-02-21 19:23:55 +0200

    added gitignore and fixed indentation

--- person_two.rb
+++ person_two.rb
@@ -1,17 +1,17 @@
 class PersonTwo
-	attr_reader :first_name, :last_name, :education_level
+  attr_reader :first_name, :last_name, :education_level
 
-	def initialize(first_name, last_name, education_level = 0)
-		@first_name = first_name
-		@last_name = last_name
-		@education_level = education_level
-	end
+  def initialize(first_name, last_name, education_level = 0)
+    @first_name = first_name
+    @last_name = last_name
+    @education_level = education_level
+  end
 
-	def full_name
-		"#{@first_name} #{@last_name}"
-	end
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
 
-	def educate!
-		@education_level += 1 if @education_level < 3
-	end
+  def educate!
+    @education_level += 1 if @education_level < 3
+  end
 end
